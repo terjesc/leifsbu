bdaea4278c4fa2dfd584e1105843ae056bafc365
Terje Schjelderup <terjesc@pvv.org>
2021-05-16 21:00:37 +0200

    Add better roads and bridges

--- src/road.rs
+++ src/road.rs
@@ -3,6 +3,7 @@ use crate::tree;
 use crate::pathfinding::{RoadNode, RoadNodeKind, RoadPath};
 
 use image::GrayImage;
+use mcprogedit::block;
 use mcprogedit::block::Block;
 use mcprogedit::material::Material;
 use mcprogedit::positioning::Axis3;
@@ -14,17 +15,65 @@ pub fn build_road(
     height_map: &GrayImage,
     road_width: i64,
 ) {
+    // Build the path segments
+    for segment in path.windows(2) {
+        let line = line::line(
+            &(segment[0].coordinates),
+            &(segment[1].coordinates),
+            road_width,
+        );
+        for position in &line {
+            tree::chop(excerpt, *position - (0, 2, 0).into());
+            tree::chop(excerpt, *position - (0, 1, 0).into());
+            tree::chop(excerpt, *position);
+            tree::chop(excerpt, *position + (0, 1, 0).into());
+            tree::chop(excerpt, *position + (0, 2, 0).into());
+        }
+
+        match (segment[0].kind, segment[1].kind) {
+            (RoadNodeKind::WoodenSupport, RoadNodeKind::WoodenSupport) => {
+                for position in &line {
+                    excerpt.set_block_at(*position, Block::dark_oak_planks());
+                    excerpt.set_block_at(*position + (0, 1, 0).into(), Block::Air);
+                    excerpt.set_block_at(*position + (0, 2, 0).into(), Block::Air);
+                }
+            }
+            (RoadNodeKind::WoodenSupport, _) | (_, RoadNodeKind::WoodenSupport) => {
+                for position in &line {
+                    excerpt.set_block_at(*position, Block::bottom_slab(Material::DarkOak));
+                    excerpt.set_block_at(*position + (0, 1, 0).into(), Block::Air);
+                    excerpt.set_block_at(*position + (0, 2, 0).into(), Block::Air);
+                }
+            }
+            (RoadNodeKind::StoneSupport, RoadNodeKind::StoneSupport) => {
+                for position in &line {
+                    excerpt.set_block_at(*position, Block::Cobblestone);
+                    excerpt.set_block_at(*position + (0, 1, 0).into(), Block::Air);
+                    excerpt.set_block_at(*position + (0, 2, 0).into(), Block::Air);
+                }
+            }
+            (RoadNodeKind::StoneSupport, _) | (_, RoadNodeKind::StoneSupport) => {
+                for position in &line {
+                    excerpt.set_block_at(*position, Block::bottom_slab(Material::Cobblestone));
+                    excerpt.set_block_at(*position + (0, 1, 0).into(), Block::Air);
+                    excerpt.set_block_at(*position + (0, 2, 0).into(), Block::Air);
+                }
+            }
+            _ => {
+                for position in &line {
+                    excerpt.set_block_at(*position - (0, 2, 0).into(), Block::Cobblestone);
+                    excerpt.set_block_at(*position - (0, 1, 0).into(), Block::Gravel);
+                    excerpt.set_block_at(*position, Block::Air);
+                    excerpt.set_block_at(*position + (0, 1, 0).into(), Block::Air);
+                    excerpt.set_block_at(*position + (0, 2, 0).into(), Block::Air);
+                }
+            }
+        }
+    }
+
     // Build the nodes
     for RoadNode { coordinates, kind, .. } in path {
         let (x, y, z) = (coordinates.0, coordinates.1, coordinates.2);
-
-        // Space above the nodes
-        tree::chop(excerpt, (x, y, z).into());
-        tree::chop(excerpt, (x, y + 1, z).into());
-        tree::chop(excerpt, (x, y + 2, z).into());
-        excerpt.set_block_at((x, y, z).into(), Block::Air);
-        excerpt.set_block_at((x, y+1, z).into(), Block::Air);
-        excerpt.set_block_at((x, y+2, z).into(), Block::Air);
 
         // Path and support at node
         match kind {
@@ -32,7 +81,9 @@ pub fn build_road(
                 tree::chop(excerpt, (x, y-1, z).into());
                 excerpt.set_block_at(
                     (x, y-1, z).into(),
-                    Block::double_slab(Material::SmoothStone)
+                    //Block::double_slab(Material::SmoothStone),
+                    //Block::Andesite,
+                    Block::BlockOfGold,
                 );
             }
             RoadNodeKind::WoodenSupport => {
@@ -45,32 +96,22 @@ pub fn build_road(
             RoadNodeKind::StoneSupport => {
                 let image::Luma([ground]) = height_map[(x as u32, z as u32)];
                 for y in ground as i64..y {
-                    tree::chop(excerpt, (x, y, z).into());
-                    excerpt.set_block_at((x, y, z).into(), Block::StoneBricks);
+                    let coordinates = (x+1, y, z).into();
+                    tree::chop(excerpt, coordinates);
+                    excerpt.set_block_at(coordinates, Block::StoneBricks);
+                    let coordinates = (x-1, y, z).into();
+                    tree::chop(excerpt, coordinates);
+                    excerpt.set_block_at(coordinates, Block::StoneBricks);
+                    let coordinates = (x, y, z+1).into();
+                    tree::chop(excerpt, coordinates);
+                    excerpt.set_block_at(coordinates, Block::StoneBricks);
+                    let coordinates = (x, y, z-1).into();
+                    tree::chop(excerpt, coordinates);
+                    excerpt.set_block_at(coordinates, Block::StoneBricks);
                 }
             }
             _ => (),
         }
     }
 
-    // Build the path segments
-    for segment in path.windows(2) {
-        let line = line::line(
-            &(segment[0].coordinates),
-            &(segment[1].coordinates),
-            road_width,
-        );
-        for position in line {
-            tree::chop(excerpt, position - (0, 2, 0).into());
-            tree::chop(excerpt, position - (0, 1, 0).into());
-            tree::chop(excerpt, position);
-            tree::chop(excerpt, position + (0, 1, 0).into());
-            tree::chop(excerpt, position + (0, 2, 0).into());
-            excerpt.set_block_at(position - (0, 2, 0).into(), Block::Cobblestone);
-            excerpt.set_block_at(position - (0, 1, 0).into(), Block::Gravel);
-            excerpt.set_block_at(position, Block::Air);
-            excerpt.set_block_at(position + (0, 1, 0).into(), Block::Air);
-            excerpt.set_block_at(position + (0, 2, 0).into(), Block::Air);
-        }
-    }
 }
